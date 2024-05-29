44d4ae68ac04f03d7793745ecc816c222d609210
Alex Nascimento <alexjose68@hotmail.com>
2024-05-29 18:48:17 -0300

    feat: Creating the application's HTML structure!

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import logoImg from './assets/logo.png';
 import './App.css'
 
 function App() {
 
   return (
-    <div>
+    <div className='container'>
       <h1>DEV FRASES</h1>
+
+      <img
+        src={logoImg}
+        alt="Logo Dev Frases"
+        className='logo'
+      />
+
+      <h2 className='title'>Categorias</h2>
+      <section className='category-area'>
+        <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bem estar</button>
+      </section>
+
+      <button className='button-frase'>Gerar Frase</button>
+
+      <h3 className='textoFrase'>Alguma frase vai vir aqui!</h3>
     </div>
   )
 }
