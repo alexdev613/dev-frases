3c034a77c358e63dcae073c1c6c89ada0a3421e7
Alex Nascimento <alexjose68@hotmail.com>
2024-05-29 21:34:39 -0300

    feat: I created an array of objects called 'nome' with some phrases and implemented a map to traverse these objects and name buttons based on a property called 'nome' of the object array, I also implemented a style for the border of the selected button from a state of a useState with conditional rendering. And I also used conditional rendering to display text on the screen from a string filled in using a useState that controls this string, giving dynamism to our application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from 'react';
+import './App.css';
+
 import logoImg from './assets/logo.png';
-import './App.css'
 
 function App() {
+
+  const [textoFrase, setTextoFrase] = useState("");
+  const [categoriaSelecionada, setCategoriaSelecionada] = useState(0);
+
+  const allFrases = [
+    {
+      id: 1,
+      nome: "Motivacionais",
+      frases: [
+        'Siga os bons e aprenda com eles.',
+        'O bom-senso vale mais do que muito conhecimento.',
+        'O riso é a menor distância entre duas pessoas.',
+        'Deixe de lado as preocupações e seja feliz.',
+        'Realiaze o obvio, pense no improvável e conquiste o impossível.',
+        'Acredite em milagres, mas não dependa deles.',
+        'A maior barreira para o sucesso é o medo do fracasso.'
+      ]
+    },
+    {
+      id: 2,
+      nome: "Bom dia",
+      frases: [
+        'Acordar de bem com a vida é o primeiro passo para ter um dia abençoado! Bom dia, família!',
+        'A melhor forma de acordar é pular da cama e se preparar para correr atrás de todos os seus sonhos! Bom dia, mundo!',
+        'Escreva em seu coração: todo dia é o melhor dia do ano!',
+        'Bom dia! Não se esqueça que a sua alma é o reflexo do sol, tão forte e brilhante quanto um girassol',
+      ]
+    }
+  ]
+
+  function handleSwitchCategory(index: number) {
+    setCategoriaSelecionada(index);
+    console.log(categoriaSelecionada)
+    return;
+  }
 
   return (
     <div className='container'>
@@ -14,13 +51,27 @@ function App() {
 
       <h2 className='title'>Categorias</h2>
       <section className='category-area'>
-        <button className='category-button'>Motivação</button>
-        <button className='category-button'>Bem estar</button>
+        {/* <button className='category-button'>Motivação</button>
+        <button className='category-button'>Bem estar</button> */}
+
+        {allFrases.map( (item, index) => (
+          <button
+            key={item.id}
+            className='category-button'
+            style={{
+              borderWidth: item.nome === allFrases[categoriaSelecionada].nome ? 5 : 0,
+              borderColor: "#1fa4db"
+            }}
+            onClick={() => handleSwitchCategory(index)}
+          >
+            {item.nome}
+          </button>
+        ))}
       </section>
 
       <button className='button-frase'>Gerar Frase</button>
 
-      <p className='textoFrase'>Alguma frase vai vir aqui!</p>
+      {textoFrase !== '' && <p className='textoFrase'>{textoFrase}</p>}
     </div>
   )
 }
